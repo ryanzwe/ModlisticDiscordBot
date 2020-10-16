5e8648b6c587542d16bb6cd196d89d316bfd23a3
Ryan Wilton <ryanwilton@live.com.au>
2020-10-16 10:03:15 +1000

    Add project files.

---
+++ ModlisticDiscordBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ModlisticDiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
