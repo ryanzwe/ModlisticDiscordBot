51d143fd6fce91281fdc1894c1293f27d5389709
Ryan Wilton <ryanwilton@live.com.au>
2020-10-16 14:39:12 +1000

    Initial commit, basic bot connection & message logging.

--- ModlisticDiscordBot/Program.cs
+++ ModlisticDiscordBot/Program.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
 
 namespace ModlisticDiscordBot
 {
     class Program
     {
-        static void Main(string[] args)
+        private DiscordSocketClient client;
+        static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+
+
+
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            client = new DiscordSocketClient();
+            client.Log += Log;
+
+            var token = "";
+            if (File.Exists("BotToken.txt")) token = File.ReadAllText("BotToken.txt");
+            else
+            {
+                Console.WriteLine("\n\n\n\nFILE NOT FOUND\n\n\n\n");
+                return;
+            }
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+            //Block this until program closes
+            await Task.Delay(-1);
         }
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine($"Modlistic: {msg.ToString()}");
+            return Task.CompletedTask;
+        }
+
     }
+
 }
